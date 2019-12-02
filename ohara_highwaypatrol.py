b53a8920c5f82c89c94cb8d505ec2462f430ecc3
ashlyn <ashlyngohara@gmail.com>
2019-12-01 23:16:49 -0600

    break assignment

--- ohara_highwaypatrol.py
+++ ohara_highwaypatrol.py
@@ -10,6 +10,7 @@ url = 'https://www.mshp.dps.missouri.gov/HP68/SearchAction?searchDate=10/31/2017
 br = mechanize.Browser()
 br.open(url)
 html = br.response().read()
+#Or, html = requests.get(TYPEURL).content
 
 #STEP 3: Make soup!
 
